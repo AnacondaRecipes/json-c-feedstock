81ffb593ffa22f98169c5e790d2a5b69b755e132
Kai Tietz <ktietz@anaconda.com>
2021-08-20 17:01:18 +0200

    add support for osx (clang)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Get an updated config.sub and config.guess
+cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* .
+
+if [[ "${target_platform}" == osx-* ]]; then
+  # turn off Werror for clang ...
+  sed -i.bak -E "s/-Werror/-Wno-error/" configure.ac
+fi
+
 bash ./autogen.sh
 
 # https://github.com/json-c/json-c/issues/406
